0bef86ea0a601ac548dd425a64d5058d3ace1847
unknown <zachdangelo100@gmail.com>
2018-08-20 20:30:43 -0400

    minor fixes, mostly regarding the CSS.

--- Backup.py
+++ Backup.py
@@ -1,10 +1,12 @@
-import os, shutil, gzip
+import os
+import shutil
+import gzip
 import datetime as dt
 
 
-def backup_database(db_conn, file_name):  #Basic database backup
+def backup_database(db_conn, file_name):  # Basic database backup
     # log -- beginning database backup.
-    file_name = os.path.join(os.getcwd(),file_name)
+    file_name = os.path.join(os.getcwd(), file_name)
     backup_path = os.path.join(os.getcwd(), 'backups')
 
     try:
@@ -21,8 +23,8 @@ def backup_database(db_conn, file_name):  #Basic database backup
 
 def compress_file(file_name):
     with open(file_name, 'rb') as f_in:
-        with gzip.open(file_name +'.gz','wb') as f_out:
-            shutil.copyfileobj(f_in,f_out)
+        with gzip.open(file_name + '.gz', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
     os.remove(file_name)
 
 
